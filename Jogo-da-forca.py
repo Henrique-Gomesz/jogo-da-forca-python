e61d7b827b0cddac07c775d6c23031908fdde70c
Henrique Gomes <henriquegomesjunqueira@gmail.com>
2021-11-21 13:03:58 -0300

    Construindo Menu

--- Jogo-da-forca.py
+++ Jogo-da-forca.py
@@ -1,4 +1,9 @@
 import random
+print("-------------------------")
+print('\033[1;35m'+'Escolha o tema para jogar'+'\033[0;0m')
+
+
+
 pais=['brasil','estados-unidos','africa-do-sul','argentina','angola','chile','coreia-do-norte','cuba','dinamarca','japao','peru','paraguai']
 animais=['baleia','cachorro','gato','foca','flamingo','hiena','golfinho','raposa','vaca','tartaruga','zebra','urubu','ovelha']
 frutas=['abacate','abacaxi','amora','banana','cacau','caqui','cereja','goiaba','framboesa','jaca','kiwi','laranja','manga','melancia','morango','pera','pitaya','tangerina','uva']
@@ -24,4 +29,4 @@ while cont!=6 and acertou==False:
     for a in range(0,len(palavra)):
         if str[a]=="_":
             acertou= False
-            A
+            
